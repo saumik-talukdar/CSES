9256a1c90b09bc16e1685b045ff40ff1f5b5a357
Saumik <saumiktalukdar956@gmail.com>
2024-01-20 10:49:25 +0600

    wierd algorithm

---
+++ Introductory Problem/wierd_algorithm.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    long long n;
+    cin >> n;
+    while (n != 1)
+    {
+        cout << n << " ";
+        if (n % 2 == 0)
+        {
+            n = n / 2;
+        }
+        else
+        {
+            n = 3 * n + 1;
+        }
+    }
+    cout << n;
+
+    return 0;
+}
